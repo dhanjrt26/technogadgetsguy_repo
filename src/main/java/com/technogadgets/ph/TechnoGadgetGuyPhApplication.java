871d0bada89b1ae642b1e49e776bc273beb8fba6
Danilo Jr Torrejos <danilotorrejosjr@gmail.com>
2022-08-20 04:27:16 +0800

    Signed-off-by: Danilo Jr Torrejos <danilotorrejosjr@gmail.com>

--- src/main/java/com/technogadgets/ph/TechnoGadgetGuyPhApplication.java
+++ src/main/java/com/technogadgets/ph/TechnoGadgetGuyPhApplication.java
@@ -2,20 +2,14 @@ package com.technogadgets.ph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 
 
 @SpringBootApplication
-public class TechnoGadgetGuyPhApplication extends SpringBootServletInitializer {
+public class TechnoGadgetGuyPhApplication {
 
 	
-
-/**
- * @param args Arguments
-*/
-
 	public static void main(String[] args){
 		SpringApplication.run(TechnoGadgetGuyPhApplication.class, args);
 	}
